161558c0573bd09c9fd0dd554288bb208cc9d7c8
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-10-10 01:02:43 +0400

    setup Admin panel

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Olla
+      </header>
+    </div>
+  );
+}
+
+export default App;
